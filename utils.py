17953bf9dc46b4edd9f113726e4107bb1b3c5aac
Eugenio Zuccarelli <11176606+ezuccarelli@users.noreply.github.com>
2020-04-29 16:47:18 -0400

    Add utility to get and save images

---
+++ utils.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import pathlib as pl
+import urllib as ul
+
+
+user = 'ezucca'
+
+
+def get_artists():
+    pass
+
+
+def get_artworks():
+    pass
+
+
+def get_images():
+    artworks = load_artworks()
+    for i, r in artworks.iloc[:3, :].iterrows():
+        ul.request.urlretrieve(r['image'], '/pool001/' + user + '/Connoisseur/Artworks/' +
+                               str(r['artist']) + '/' + str(r['name']) + r['image'][-4:])
+
+
+def make_directories(artists):
+    for artist in artists:
+        pl.Path('/pool001/' + user + '/Connoisseur/Artworks/'+str(artist)).mkdir(parents=True, exist_ok=True)
+
+
+def load_artists():
+    return pd.read_csv('/pool001/' + user + '/Connoisseur/Data/artists.csv')
+
+
+def load_artworks():
+    return pd.read_csv('/pool001/' + user + '/Connoisseur/Data/artworks.csv')
