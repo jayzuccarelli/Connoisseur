23008695e9b257478dd33453513f1372c4e839e9
Eugenio Zuccarelli <11176606+ezuccarelli@users.noreply.github.com>
2020-04-30 14:13:17 -0400

    Clean up code

--- main.py
+++ main.py
@@ -1,19 +1,13 @@
 import numpy as np
-np.random.seed(12321)  # for reproducibility
+from tensorflow.keras import backend as K
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
 from tensorflow.keras.layers import Convolution2D
 from tensorflow.keras.layers import MaxPooling2D
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from io import BytesIO
-from keras import backend as K
-import urllib
-import keras
-import tensorflow as tf
-import os
 import pathlib
-path_to_data_dir = '../Datasets/'
+import tensorflow as tf
 #TODO: Clean up libraries and reorder
 
 
@@ -23,7 +17,6 @@ img_rows, img_cols = 42, 28
 nb_epoch = 3
 batch_size = 64
 K.common.set_image_dim_ordering('th')
-# input image dimensions
 
 
 BATCH_SIZE = 32
@@ -34,11 +27,11 @@ IMG_WIDTH = 224
 def main():
     my_data_dir = '/pool001/ezucca/Connoisseur/Artworks'
     data_dir = pathlib.Path(my_data_dir)
-    CLASS_NAMES = np.array([item.name for item in data_dir.glob('*')])
+    CLASS_NAMES = [item.name for item in data_dir.glob('*')]
     BATCH_SIZE = 32
     STEPS_PER_EPOCH = np.ceil(len([item.name for item in data_dir.glob('*')]) / BATCH_SIZE)
     image_generator = ImageDataGenerator(rescale=1. / 255)
-    BATCH_SIZE = 32
+    BATCH_SIZE = 30
     IMG_HEIGHT = 200
     IMG_WIDTH = 200
     nb_epoch = 3
@@ -46,14 +39,10 @@ def main():
                                                          batch_size=BATCH_SIZE,
                                                          shuffle=True,
                                                          target_size=(IMG_HEIGHT, IMG_WIDTH),
-                                                         classes=list(CLASS_NAMES))
-    train_data_gen = image_generator.flow_from_directory(directory=str(data_dir),
-                                                         batch_size=BATCH_SIZE,
-                                                         shuffle=True,
-                                                         target_size=(IMG_HEIGHT, IMG_WIDTH),
-                                                         classes=list(CLASS_NAMES))
+                                                         classes=CLASS_NAMES)
+
     X, class_label = next(train_data_gen)
-    # Then just use tf.split on this #TODO: Separate train, validation and test
+    train_X, validation_X, test_X = tf.split(X, [0.7*X.shape[0], 0.2*X.shape[0], 0.1*X.shape[0]], 0)
 
     inputs = Input(shape=(IMG_WIDTH, IMG_HEIGHT, 3))
     step_1 = Convolution2D(filters=8, kernel_size=(2, 2), activation='relu', padding='same', data_format="channels_last")(inputs) #TODO: Set channels parameter
