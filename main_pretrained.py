da5f0c86a9fb0b93648cfa5f8e593fef4c0d34af
killian-f <killianjfarrell@gmail.com>
2020-05-05 10:45:06 -0400

    Pretrained Architecture

---
+++ main_pretrained.py
@@ -0,0 +1,57 @@
+import numpy as np
+import os as os
+import pathlib as pl
+import keras as ks
+from PIL import ImageFile
+
+
+global USER, NUM_GPUS
+USER = 'killianf'
+NUM_GPUS = 2
+
+def main():
+    LOCATION = '/pool001/' + USER + '/Connoisseur/Artworks'
+    PATH = pl.Path(LOCATION)
+    IMAGES = PATH
+    CLASSES = [item.name for item in PATH.glob('*')]
+    BATCH_SIZE = 64
+    EPOCHS = 10
+    STEPS_PER_EPOCH = 256
+    IMG_WIDTH = 200
+    IMG_HEIGHT = 200
+    ImageFile.LOAD_TRUNCATED_IMAGES = True
+    image_generator = ks.preprocessing.image.ImageDataGenerator(rescale=1. / 255,
+                                                                horizontal_flip=True,
+                                                                vertical_flip=True)
+    train_data_gen = image_generator.flow_from_directory(directory=IMAGES,
+                                                         batch_size=BATCH_SIZE,
+                                                         shuffle=True,
+                                                         target_size=(IMG_HEIGHT, IMG_WIDTH),
+                                                         classes=CLASSES)
+    inputs = ks.layers.Input(shape=(IMG_HEIGHT, IMG_WIDTH, 3))
+    in_norm = ks.layers.BatchNormalization()(inputs)
+    step_1 = ks.layers.Convolution2D(filters=32, kernel_size=(4, 4), activation='relu',
+                                     padding='same')(in_norm)
+    step_2 = ks.layers.MaxPooling2D(pool_size=(2, 2), padding='same')(step_1)
+    step_3 = ks.layers.Convolution2D(filters=16, kernel_size=(8, 8), activation='relu',
+                                     padding='same')(step_2)
+    step_4 = ks.layers.MaxPooling2D(pool_size=(2, 2), padding='same')(step_3)
+    step_5 = ks.layers.Convolution2D(filters=16, kernel_size=(2, 2), activation='relu',
+                                     padding='same')(step_4)
+    step_6 = ks.layers.MaxPooling2D(pool_size=(2, 2), padding='same')(step_5)
+    step_7 = ks.layers.Flatten()(step_6)
+    step_8 = ks.layers.Dense(128, activation='relu')(step_7)
+    step_9 = ks.layers.Dropout(rate=0.2)(step_8)
+    output = ks.layers.Dense(len(CLASSES), activation='softmax')(step_9)
+    model = ks.models.Model(inputs=inputs, outputs=output)
+    model = ks.utils.multi_gpu_model(model, gpus=NUM_GPUS)
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=ks.optimizers.Adadelta(),
+                  metrics=['accuracy'])
+    model.fit_generator(train_data_gen, epochs=EPOCHS, steps_per_epoch=STEPS_PER_EPOCH, verbose=1)
+    if ks.backend.backend() == 'tensorflow':
+        ks.backend.clear_session()
+    print('Done.')
+
+if __name__ == '__main__':
+    main()
---
+++ main_pretrained.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=connoisseur
+#SBATCH --output=output.txt
+#SBATCH --error=errors.txt
+#SBATCH -p sched_mit_sloan_gpu
+#SBATCH --gres=gpu:2
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=10
+#SBATCH --mem-per-cpu=12G
+#SBATCH --time=1-00:00:00
+module load python/3.6.3
+module load sloan/python/modules/python-3.6/tensorflow/1.9.0/gpu
+python3.6 main_pretrained.py
