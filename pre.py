9990ac3ed37c2067fe7956bfa4e769c97c784a67
Eugenio Zuccarelli <11176606+ezuccarelli@users.noreply.github.com>
2020-05-07 10:44:39 -0400

    Increase lr for Adam

--- pre.py
+++ pre.py
@@ -19,7 +19,7 @@ def main():
     CLASSES = [item.name for item in PATH.glob('*')]
 
     # Parameters
-    BATCH_SIZE = 32
+    BATCH_SIZE = 128
     EPOCHS = 1000
     STEPS_PER_EPOCH = np.ceil(len(CLASSES) / BATCH_SIZE)
     IMG_WIDTH = 256
@@ -45,24 +45,19 @@ def main():
     # train_X, validation_X, test_X = tf.split(X, [0.7*X.shape[0], 0.2*X.shape[0], 0.1*X.shape[0]], 0)
 
     # Model
-    inputs = ks.layers.Input(shape=(IMG_WIDTH, IMG_HEIGHT, 3)) #TODO: Try None, None - Should it be width and height or viceversa?
-    step_1 = ks.layers.Convolution2D(filters=16, kernel_size=(3, 3), activation='relu',
-                                     padding='same', data_format="channels_last")(inputs)
-    step_2 = ks.layers.MaxPooling2D(pool_size=(2, 2), strides=2, padding='same')(step_1)
-    step_3 = ks.layers.Convolution2D(filters=32, kernel_size=(5, 5), activation='relu',
-                                     padding='same', data_format="channels_last")(step_2)
-    step_4 = ks.layers.MaxPooling2D(pool_size=(2, 2), padding='same', strides=2)(step_3)
-    step_5 = ks.layers.Flatten()(step_4)
-    step_6 = ks.layers.Dense(256, activation='relu')(step_5)
-    step_7 = ks.layers.Dense(128, activation='relu')(step_6)
-    step_8 = ks.layers.Dropout(rate=0.5)(step_7)
-    output = ks.layers.Dense(len(CLASSES), activation='softmax')(step_8)
+    inputs = ks.layers.Input(shape=(IMG_WIDTH, IMG_HEIGHT, 3))  #TODO: Try None, None - Should it be width and height or viceversa?
+    step_1 = ks.applications.VGG19(weights='imagenet', include_top=False)(inputs)
+    step_2 = ks.layers.Flatten()(step_1.outputs)
+    step_3 = ks.layers.Dense(256, activation='relu')(step_2)
+    step_4 = ks.layers.Dense(128, activation='relu')(step_3)
+    step_5 = ks.layers.Dropout(rate=0.5)(step_4)
+    output = ks.layers.Dense(len(CLASSES), activation='softmax')(step_5)
 
     model = ks.models.Model(inputs=inputs, outputs=output)
 
     model = ks.utils.multi_gpu_model(model, gpus=NUM_GPUS)
 
-    optimizer = ks.optimizers.Adam(lr=0.1)
+    optimizer = ks.optimizers.Adam(lr=0.001)
 
     model.compile(loss='categorical_crossentropy',
                   optimizer=optimizer,
